08641a81658df591a506d8342835539d67268ece
Gurudath G M <princegguruk@gmail.com>
2024-07-19 16:38:36 +0530

    Update ColorCoderTest.cs

--- ColorCoderTest.cs
+++ ColorCoderTest.cs
@@ -6,18 +6,18 @@ namespace TelCo.ColorCoder
 {
     class ColorCoderTest
     {
-        public static void testGetColorFromPairNumber(int pairNumber)
+        public static void testGetColorFromPairNumber(int pairNumber, Color expectedMajorColor, Color expectedMinorColor)
         {
             ColorPair testPair1 = ColorCoder.GetColorFromPairNumber(pairNumber);
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.White);
-            Debug.Assert(testPair1.minorColor == Color.Brown);
+            Debug.Assert(testPair1.majorColor == expectedMajorColor);
+            Debug.Assert(testPair1.minorColor == expectedMinorColor);
         }
-        public static void GetPairNumberFromColor(ColorPair testPair2)
+        public static void GetPairNumberFromColor(ColorPair testPair2, int expectedPairNumber)
         {
             int pairNumber = ColorCoder.GetPairNumberFromColor(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
-            Debug.Assert(pairNumber == 18);
+            Debug.Assert(pairNumber == expectedPairNumber);
         }
     }
 }
